dffb24989bd9dd94dc15dbf7244d6b9035fdd36c
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-03-26 17:04:52 +0800

    Added all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'roles', loadChildren: () => import('./roles/roles.module').then(m => m.RolesModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }, { path: 'product', loadChildren: () => import('./product/product.module').then(m => m.ProductModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/roles/roles.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RolesRoutingModule } from './roles-routing.module';
+import { RolesComponent } from './roles.component';
+
+@NgModule({
+  declarations: [
+    RolesComponent
+  ],
+  imports: [
+    CommonModule,
+    RolesRoutingModule,
+  ]
+})
+export class RolesModule { }
